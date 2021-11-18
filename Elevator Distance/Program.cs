d80034e2831aa55916ca56126c5f96f4c333c785
Misha Biryuk <vovann7093@gmail.com>
2021-11-18 19:56:47 +0200

    Elevator Distance

---
+++ Elevator Distance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Elevator_Distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {2, 15, 13, 2, 4, 4, 4, 5, 6 };
+            Console.WriteLine(ElevatorDistance(array));
+        }
+        public static int ElevatorDistance(int[] array)
+        {
+            int result1 = 0, result = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] - array[i + 1] < 0)
+                {
+                    result1 = array[i] - array[i + 1];
+                    result1 *= -1;
+                }
+                else
+                {
+                    result1 = array[i] - array[i + 1];
+                }
+                result += result1;
+            }
+            return result;
+        }
+    }
+}
