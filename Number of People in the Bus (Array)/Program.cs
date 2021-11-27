c7c98c2ecbf7b771be91aa3f14cc2c582014f13d
Misha Biryuk <vovann7093@gmail.com>
2021-11-27 13:42:49 +0200

    First commit

---
+++ Number of People in the Bus (Array)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Number_of_People_in_the_Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] people = { {10, 0}, {3, 5}, {5, 8} };
+            Console.WriteLine(Number(people));
+        }
+        public static int Number(int[,] peopArrayInOut)
+        {
+            int peopleGetIntoBus = 0;
+            int peopleGetOffTheBus = 0;
+            for (int i = 0; i < peopArrayInOut.Length / 2; i++)
+            {
+                peopleGetIntoBus += peopArrayInOut[i, 0];
+                peopleGetOffTheBus += peopArrayInOut[i, 1];
+            }
+            return peopleGetIntoBus - peopleGetOffTheBus;   // 5
+        }
+    }
+}
