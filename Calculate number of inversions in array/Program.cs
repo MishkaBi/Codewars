bbfce301a658040940308aa1de5bc7a793b07858
Misha Biryuk <vovann7093@gmail.com>
2021-12-11 20:35:30 +0200

    first commit

---
+++ Calculate number of inversions in array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Calculate_number_of_inversions_in_array
+{
+    class Program
+    {
+        public static int CountInversions(int[] array)
+        {
+            int counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int tmp = array[j];
+                        array[j] = array[i];
+                        array[i] = tmp;
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 4, 3, 2, 1};
+
+            Console.WriteLine(CountInversions(array)); // 6
+        }
+    }
+}
